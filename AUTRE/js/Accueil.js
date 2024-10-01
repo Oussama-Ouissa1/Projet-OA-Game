6bbe003d116bf70a171d17b3bc8af61153d4afd6
Oussama-Ouissa1 <oussamaaouissa2006@gmail.com>
2024-10-01 17:02:18 +0100

    OA Game

--- AUTRE/js/Accueil.js
+++ AUTRE/js/Accueil.js
@@ -11,3 +11,22 @@ function ajoutDiv(qte){
 
 ajoutDiv(3);
 
+
+
+function openBurger(){
+    const navbar = document.querySelector('.navbarBurger1')
+    navbar.style.display = 'block'
+    const nv1 = document.querySelector('.nv1')
+    nv1.style.display = 'none'
+    const nv2 = document.querySelector('.nv2')
+    nv2.style.display = 'block'
+}
+
+function closeBurger(){
+    const navbar = document.querySelector('.navbarBurger1')
+    navbar.style.display = 'none'
+    const nv2 = document.querySelector('.nv2')
+    nv2.style.display = 'none'
+    const nv1 = document.querySelector('.nv1')
+    nv1.style.display = 'block'
+}
