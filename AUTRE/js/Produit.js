1859c91c38c86b5e81ac0ff138f80cf2a0f5a0d8
Oussama-Ouissa1 <oussamaaouissa2006@gmail.com>
2024-10-02 18:05:30 +0100

    OA Game

--- AUTRE/js/Produit.js
+++ AUTRE/js/Produit.js
@@ -44,3 +44,24 @@ function accessoirDiv(qte){
     }
 }
 accessoirDiv(5);
+
+
+
+
+function openBurger(){
+    const navbar = document.querySelector('.navbarBurger1')
+    navbar.style.display = 'block'
+    const nv1 = document.querySelector('.nv1')
+    nv1.style.display = 'none'
+    const nv2 = document.querySelector('.nv2')
+    nv2.style.display = 'block'
+}
+
+function closeBurger(){
+    const navbar = document.querySelector('.navbarBurger1')
+    navbar.style.display = 'none'
+    const nv2 = document.querySelector('.nv2')
+    nv2.style.display = 'none'
+    const nv1 = document.querySelector('.nv1')
+    nv1.style.display = 'block'
+}
